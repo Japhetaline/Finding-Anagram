a5d729c9be26265873e7f4a08fe6e5c6f4a8c3e7
Japhetaline <Japhetdamassoh@gmail.com>
2022-05-30 16:39:07 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Check if a word is an anagrams 
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
+
+
+str1 = ("listen")
+str2 = ("silent")
+print(sorted(str1) == sorted(str2))
+
+
